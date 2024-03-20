991a5ac82133f5101227ad18be7713a0fd0e0d46
Vidyoot Senthil <vidyoots@gmail.com>
2024-03-19 20:10:07 -0500

    added open api docs link and updated hover animations

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,21 +2,26 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import tailwind from "@astrojs/tailwind";
 
+// FIX the code issue to add a navbar item to the top of the page
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://docs.trieve.ai",
   integrations: [starlight({
     title: 'Trieve Docs',
-	customCss: ['./src/tailwind.css'],
+	  customCss: ['./src/tailwind.css'],
     social: {
       github: 'https://github.com/devflowinc/trieve',
       twitter: 'https://twitter.com/trieveai',
       discord: 'https://discord.gg/jWdZc3XSWB',
-      matrix: 'https://matrix.to/#/#trieve-general:matrix.zerodao.gg'
+      matrix: 'https://matrix.to/#/#trieve-general:matrix.zerodao.gg',
     },
     logo: {
       src: './src/assets/trieve-logo.png',
       alt: 'Trieve Logo'
+    },
+    components: {
+      SocialIcons: './src/components/NavbarItem.astro',
     },
     editLink: {
       baseUrl: 'https://github.com/devflowinc/trieve-docs/edit/main'
