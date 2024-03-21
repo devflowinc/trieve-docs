4b8bfc3172426b0349eca02d633f527f70359d18
Vidyoot Senthil <vidyoots@gmail.com>
2024-03-21 12:25:24 -0500

    updated starting page

--- astro.config.mjs
+++ astro.config.mjs
@@ -30,7 +30,7 @@ export default defineConfig({
       // Each item here is one entry in the navigation menu.
       {
         label: 'Example Guide',
-        link: '/guides/example/'
+        link: '/guides/homepage/'
       }]
     }, {
       label: 'Reference',
